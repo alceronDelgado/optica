e788a517091aa8a2d4888808141bc350204ecce2
alceronDelgado <luis_aceron@soy.sena.edu.co>
2025-02-19 21:03:50 -0500

    implementado estilos en login

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 require_once 'config/conn.php';
@@ -8,58 +8,64 @@ require_once 'src/querys/functionsSelects.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<?php require_once 'assets/templates/head.php'; ?>
+    <?php require_once 'assets/templates/head.php'; ?>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clinica de la visión</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
 
-<div class="container">
-    <span>Iniciar sesión:</span>
-    <div class="form">
-        <form action="" method="post" id="formLogin">
-                <div class="input-field col s12">
-                    <label for="rolesId">Rol:</label>
-                    <select name="rolesId" id="rol_id">
-                        
-                    <?php 
-                        $roles = selectRols();
-                        foreach ($roles as $rolesInputs) {
-                    ?>
-                        <option value="<?php echo $rolesInputs['rolesId']; ?>">
-                            <?php echo $rolesInputs['nombreRol']; ?>
-                        </option>
-                    <?php }?>
-                </select>
-                </div>
-            <div class="user input-field col s12">
-                <label for="">Documento:</label>
-                <input type="number" name="user" id="usu_docum">
+    <div class="container">
+        <div class="title">
+            <h1>Clinica la visión</h1>
+        </div>
+        <div class="row">
+            <!-- Contenedor de la Imagen -->
+            <div class="col s12 m6">
+                <img src="assets/img/optometra.svg" alt="Imagen de ejemplo" class="responsive-img">
             </div>
-            <div class="password input-field col s12">
-                <label for="">Contraseña:</label>
-                <input type="password" name="password" id="usu_clave">
+
+            <!-- Contenedor del Formulario -->
+            <div class="col s12 m6">
+                <span class="col s12 center-align">Iniciar sesión</span>
+                <form action="" method="post" id="formLogin" class="">
+                    <div class="input-field col s12">
+                        <label for="rolesId">Rol:</label>
+                        <select name="rolesId" id="rol_id">
+                            <?php
+                            $roles = selectRols();
+                            foreach ($roles as $rolesInputs) {
+                            ?>
+                                <option value="<?php echo $rolesInputs['rolesId']; ?>">
+                                    <?php echo $rolesInputs['nombreRol']; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="user input-field col s12">
+                        <label for="">Documento:</label>
+                        <input type="number" name="user" id="usu_docum">
+                    </div>
+                    <div class="password input-field col s12">
+                        <label for="">Contraseña:</label>
+                        <input type="password" name="password" id="usu_clave">
+                    </div>
+                    <div class="buttom input-field col s12 center-align">
+                        <button type="submit" id="submit" class="btn waves-effect waves-light">ingresar</button>
+                    </div>
+                </form>
             </div>
-            <div class="buttom input-field col s12">
-                <button type="submit" id="submit" class="btn waves-effect waves-light">ingresar</button>
-            </div> 
-        </form>
+        </div>
     </div>
-</div>
 
+    <?php require_once 'assets/templates/imports.php'; ?>
 
-
-<?php 
-
-require_once 'assets/templates/imports.php';
-
-?>
-
-<script src="assets/js/index.js"></script>
+    <script src="assets/js/index.js"></script>
 </body>
 </html>
