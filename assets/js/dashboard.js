e9d2b28fcb32289e237fd6520e31b0cdd577ae01
alceronDelgado <luis_aceron@soy.sena.edu.co>
2025-03-12 22:51:54 -0500

    Implementado insert into con tabla paciente_hobbies

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,72 +1,4 @@
 $(document).ready( function () {
-    // let myTable = $('#tabla').DataTable({
-    //   "pageLength": 5,  // Número de registros por página por defecto
-    //   "lengthMenu": [ [5, 10, 15, -1], [5, 10, 15, "Todos"] ],  // Mostrar opciones de 5, 10, 15 registros
-    //   "language": {
-    //     "sSearch": "Buscar:",  // Texto del campo de búsqueda
-    //     "lengthMenu": "Mostrar _MENU_ registros por página",  
-    //     "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",  
-    //     "infoEmpty": "Mostrando 0 a 0 de 0 registros",  
-    //     "infoFiltered": "(filtrado de _MAX_ registros)",  
-    //     "paginate": {
-    //       "first": "Primero",  
-    //       "previous": "Anterior", 
-    //       "next": "Siguiente",  
-    //       "last": "Último"  
-    //     }
-    //   },
-    //   "serverSide": false,
-    //   "autoWidth": false,
-    //   "responsive": true,
-    //   "processing": true,
-    //   "deferRender": true,
-    //   "ajax": {
-    //     "url": "querys/functionsSelectsTwo.php",
-    //     "data": {codigoSelect: 1},
-    //     "datatype": "json",
-    //     "dataSrc": function (data) { 
-    //           console.log(data);  
-    //           return data || []; 
-    //       },
-
-    //     "error": function(xhr, status, error) {
-    //             console.error("Error en la solicitud AJAX:", error);
-    //         },
-    //     "cache": false,
-    //     "type": "POST",
-    //     },
-    //     "columns":[
-    //         // columnas
-    //         {
-    //             "data":"documento"
-    //         },
-    //         {
-    //             "data": "nombrePaciente"
-    //         },
-    //         {
-    //             "data": "apellidoPaciente"
-    //         },
-    //         {
-    //             "data": "estrato"
-    //         },
-    //         {
-    //             "data": null,
-    //             "render": function(data) {
-    //                 return `
-    //                 <div class="text-center"><div class="btn-group" role="group" aria-label="Button group"><button id="btnDetalle" class="btn btn-info" type="button" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16" height="16"><path fill="white" d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" /></svg></button><button id="btnEditar" class="btn btn-primary editbtn" type="button" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></button><button id="btnEliminar" data-id="${data.codigo}" "class="btn btn-info eliminarbtn" type="button" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></button><button id="btnAdd" data-id="${data.codigo}" class="btn" style="background-color: yellow; color: white" type="button">
-    //                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" viewBox="0 0 512 512">
-    //                     <path d="M280 64h-48c-13.3 0-24 10.7-24 24v72c0 13.3 10.7 24 24 24h48c13.3 0 24-10.7 24-24V88c0-13.3-10.7-24-24-24zM128 32h256c8.8 0 16 7.2 16 16v400c0 8.8-7.2 16-16 16H128c-8.8 0-16-7.2-16-16V48c0-8.8 7.2-16 16-16z"/>
-    //                 </svg>
-    //             </button></div></div>
-    //             `;
-    //             }
-                
-    //         }
-    //     ]
-      
-    // });
-
-
     //Título de la tabla.
     let myTable = $('#tabla').DataTable({
       "pageLength": 5,  // Número de registros por página por defecto
@@ -204,7 +136,7 @@ $(document).ready( function () {
     let documentoPaciente = $('#documentoPaciente').val();
     let nombrePaciente = $('#nombrePaciente').val();
     let apellidoPaciente = $('#apellidoPaciente').val();
-    let dirreccionPaciente  = $('#dirreccionPaciente').val();
+    let direccionPaciente  = $('#direccionPaciente').val();
     let telefonoPaciente = $('#telefonoPaciente').val();
     let emailPaciente = $('#emailPaciente').val();
     let estratoPaciente = radioEstrato;
@@ -217,7 +149,7 @@ $(document).ready( function () {
       documentoPaciente,
       nombrePaciente,
       apellidoPaciente,
-      dirreccionPaciente,
+      direccionPaciente,
       telefonoPaciente,
       emailPaciente,
       generoPaciente,
@@ -253,6 +185,9 @@ $(document).ready( function () {
               confirmButtonText: 'Aceptar',
               showCancelButton: false,
               });
+              //Cierro Modal.
+              $('#myModal').fadeOut();
+              
           }
         });
       }
--- src/querys/functionsInsert.php
+++ src/querys/functionsInsert.php
@@ -29,39 +29,84 @@ function insertNewUser($clave,$nombre,$rol, $passwordInput){
 function newPaciente($newData){
 
     global $pdo;
+
     $documentoPaciente = $newData['documentoPaciente'];
     $nombrePaciente = $newData['nombrePaciente'];
     $apellidoPaciente = $newData['apellidoPaciente'];
-    $dirreccionPaciente = $newData['dirreccionPaciente'];
-    $telefonoPaciente = $newData['telefonoPaciente'];
+    $direccionPaciente = $newData['direccionPaciente'];
+    $telefonoPaciente = (is_numeric($newData['telefonoPaciente'])) ? $newData['telefonoPaciente'] : null;
+    //Uso filter var para validar si el viene el campo vacio o null o si no es numerico, no aplico la condición de vacio porque ya lo valida el propio filter_var ya que si viene null o vacio, el devolvera false, empty hace lo mismo.
+    $emailPaciente = (filter_var($newData['emailPaciente'],FILTER_VALIDATE_EMAIL)) ? $newData['emailPaciente'] : null;
     $emailPaciente = $newData['emailPaciente'];
     $generoPaciente = $newData['generoPaciente'];
     $estratoPaciente = $newData['estratoPaciente'];
-
-    //Esto es un arreglo.
-    //TODO: buscar como insertar los arreglos en el sql en la tabla pacientes_hobbies.
-    $hobbiesPaciente = $newData['hobbiesPaciente'];
-
-    
-
-    $sql = "INSERT INTO paciente (pac_docum,pac_nombre,pac_apellido,pac_direccion,pac_telefono,pac_email,gen_id,estr_id) VALUES (:pac_docum,:pac_nombre,:pac_apellido,:pac_direccion,:pac_telefono,:pac_email,:gen_id,:estr_id)";
-
-    $insertNewPacient = $pdo->prepare($sql);
-
-    $insertNewPacient->bindParam(':pac_docum',$documentoPaciente,PDO::PARAM_INT);
-    $insertNewPacient->bindParam(':pac_nombre',$nombrePaciente);
-    $insertNewPacient->bindParam(':pac_apellido',$apellidoPaciente);
-    $insertNewPacient->bindParam(':pac_direccion',$dirreccionPaciente);
-    $insertNewPacient->bindParam(':pac_telefono',$telefonoPaciente);
-    $insertNewPacient->bindParam(':pac_email',$emailPaciente);
-    $insertNewPacient->bindParam(':gen_id',$generoPaciente);
-    $insertNewPacient->bindParam(':estr_id',$estratoPaciente);
+    $documentoPaciente = (filter_var($newData['documentoPaciente'], FILTER_VALIDATE_INT)) ? $newData['documentoPaciente'] : null;
 
 
-    if ($insertNewPacient->execute()) {
-        $paciente = json_encode(["success" => "Registro exitoso"]);
+    //Esto es un arreglo. Si el arreglo no está vacio que me guarde la info enviada x POST a esa variable, en caso contrario, que me guarde un arreglo vacio sin más.
+    $hobbiesPaciente = (!empty($newData['hobbiesPaciente'])) ? $newData['hobbiesPaciente'] : [];
+
+    try {
+            //Como vamos a insertar datos en multiples tablas, lo recomendable en pdo es usar la palabra reservada getTransation, esto hace que si hay un error, se puede revertir las operaciones si alguna falla.
+            $pdo->beginTransaction();
+
+            $sql = "INSERT INTO paciente (pac_docum,pac_nombre,pac_apellido,pac_direccion,pac_telefono,pac_email,gen_id,estr_id) VALUES (:pac_docum,:pac_nombre,:pac_apellido,:pac_direccion,:pac_telefono,:pac_email,:gen_id,:estr_id)";
+
+            $insertNewPacient = $pdo->prepare($sql);
         
+            $insertNewPacient->bindParam(':pac_docum',$documentoPaciente,PDO::PARAM_INT);
+            $insertNewPacient->bindParam(':pac_nombre',$nombrePaciente);
+            $insertNewPacient->bindParam(':pac_apellido',$apellidoPaciente);
+            $insertNewPacient->bindParam(':pac_direccion',$direccionPaciente);
+            $insertNewPacient->bindParam(':pac_telefono',$telefonoPaciente);
+            $insertNewPacient->bindParam(':pac_email',$emailPaciente);
+            $insertNewPacient->bindParam(':gen_id',$generoPaciente,PDO::PARAM_INT);
+            $insertNewPacient->bindParam(':estr_id',$estratoPaciente,PDO::PARAM_INT);
+
+
+            if (!$insertNewPacient->execute()) {
+                throw new Exception('Error al insertar paciente');
+            }
+
+            //Antes de usar implode recomendable validar si los datos que vamos a unir en una cadena de texto sea de tipo arreglo. 
+            if (!empty($hobbiesPaciente)) {
+                $placeHolders = [];
+                $values = [];
+                foreach($hobbiesPaciente as $hobby){
+                    //Se colocan 2 ? que hacen referencia al número de columnas, si tenemos 5 columnas colocamos (?,?,?,?,?).
+                    $placeHolders[] = '(?,?)';
+                    $values[] = $documentoPaciente;
+                    $values[] = $hobby;
+                }
+
+
+                $pacienteHobies = "INSERT INTO paciente_hobbies (pac_id,hob_id) VALUES".implode(", ",$placeHolders);
+
+
+                $pacienteHobiesSql = $pdo->prepare($pacienteHobies);
+
+                //Todo: Se aplica pero su conocimiento es bajo, investigar.
+                foreach ($values as $inicio => $value) {
+                    $pacienteHobiesSql->bindValue($inicio+1,$value,PDO::PARAM_INT);
+                }
+
+                //Si es diferente de true, mostrar un mensaje de error.
+                if (!$pacienteHobiesSql->execute()) {
+                    throw new Exception('Error al insertar hobby');
+                }
+            }
+
+            $pdo->commit();
+
+            $paciente = json_encode(["success" => "Registro exitoso"]);
+        
+    } catch (Exception $th) {
+        //En caso de que haya un error en la ejecución de la consulta, hago un rollback al commit que hice de la transacción.
+        $pdo->rollBack();
+
+        $paciente = json_encode(['error' => $th->getMessage()]);
     }
+
     return $paciente;
 
 }
@@ -76,6 +121,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         case 1:
             
             //La idea de esto es guardarlo en una variable y retornarlo para enviarlo al success del ajax.
+            
+            
+            
+
             $data = newPaciente($newData);
             echo json_encode(["success" => "Registro exitoso","data" => $data]);
             break;
